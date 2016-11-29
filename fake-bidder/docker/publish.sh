90c9de59ffa0bbf11df228d8f4ed4dddb7baa4c0
Egor Ilchenko <ilchenko_ei@bw-sw.com>
2016-11-29 12:13:35 +0700

    Ticket #2673.
    Check if version passed to script

--- fake-bidder/docker/publish.sh
+++ fake-bidder/docker/publish.sh
@@ -2,6 +2,12 @@
 
 # Build and publish docker image to bw docker registry.
 
+if [ "$#" -ne 1 ];
+then
+  echo "Usage: $0 version" >&2
+  exit 1
+fi
+
 docker build -t private/bidder-mocks . --build-arg version=$1
 docker login -u rtb-dev-team -p rtb-dev-team-password ci.bw-sw.com:5000
 docker tag private/bidder-mocks ci.bw-sw.com:5000/rtb-dev-team/bidder-mocks:$1
--- fake-bidder/docker/run.sh
+++ fake-bidder/docker/run.sh
@@ -2,6 +2,12 @@
 
 # Pulls and starts docker container.
 
+if [ "$#" -ne 1 ];
+then
+  echo "Usage: $0 version" >&2
+  exit 1
+fi
+
 docker login -u rtb-dev-team -p rtb-dev-team-password ci.bw-sw.com:5000
 docker pull ci.bw-sw.com:5000/rtb-dev-team/bidder-mocks:$1
 docker stop bidder-mocks || true && docker rm bidder-mocks || true
